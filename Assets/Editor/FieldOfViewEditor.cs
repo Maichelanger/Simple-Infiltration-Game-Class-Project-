3291d5e636175cfdbdaffe84cdb307a71445f541
Maichelanger <miguelangelcarrascobobo@hotmail.com>
2024-02-21 14:35:17 +0100

    Added FOV to enemies
    Also added visible gizmos for the FOV on an Editor folder. Credits to "Comp-3 Interactive" on Youtube for all the FOV stuff.

---
+++ Assets/Editor/FieldOfViewEditor.cs
@@ -0,0 +1,33 @@
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(FieldOfView))]
+public class FieldOfViewEditor : Editor
+{
+    private void OnSceneGUI()
+    {
+        FieldOfView fov = (FieldOfView)target;
+        Handles.color = Color.white;
+        Handles.DrawWireArc(fov.transform.position, Vector3.up, Vector3.forward, 360, fov.viewRadius);
+
+        Vector3 viewAngleA = DirFromAngle(fov.transform.eulerAngles.y, -fov.viewAngle / 2);
+        Vector3 viewAngleB = DirFromAngle(fov.transform.eulerAngles.y, fov.viewAngle / 2);
+
+        Handles.color = Color.yellow;
+        Handles.DrawLine(fov.transform.position, fov.transform.position + viewAngleA * fov.viewRadius);
+        Handles.DrawLine(fov.transform.position, fov.transform.position + viewAngleB * fov.viewRadius);
+
+        if(fov.playerInSight)
+        {
+            Handles.color = Color.green;
+            Handles.DrawLine(fov.transform.position, fov.playerRef.transform.position);
+        }
+    }
+
+    private Vector3 DirFromAngle(float eulerY, float angleInDegrees)
+    {
+        angleInDegrees += eulerY;
+
+        return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), 0, Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
+    }
+}
---
+++ Assets/Scripts/Enemy/FieldOfView.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using UnityEngine;
+
+public class FieldOfView : MonoBehaviour
+{
+    public float viewRadius; [Range(0, 360)]
+    public float viewAngle;
+    public bool playerInSight;
+
+    [HideInInspector] public GameObject playerRef;
+
+    [SerializeField] private LayerMask targetMask;
+    [SerializeField] private LayerMask obstacleMask;
+    
+    private void Start()
+    {
+        playerRef = GameObject.FindGameObjectWithTag("Player");
+        StartCoroutine(FOVRoutine());
+    }
+
+    private void FieldOfViewCheck()
+    {
+        Collider[] rangeChecks = Physics.OverlapSphere(transform.position, viewRadius, targetMask);
+
+        if (rangeChecks.Length > 0)
+        {
+            Transform target = rangeChecks[0].transform;
+            Vector3 dirToTarget = (target.position - transform.position).normalized;
+
+            if (Vector3.Angle(transform.forward, dirToTarget) < viewAngle / 2)
+            {
+                float distanceToTarget = Vector3.Distance(transform.position, target.position);
+
+                if (!Physics.Raycast(transform.position, dirToTarget, distanceToTarget, obstacleMask))
+                {
+                    playerInSight = true;
+                }
+                else
+                {
+                    playerInSight = false;
+                }
+            }
+            else
+            {
+                playerInSight = false;
+            }
+        }
+        else if (playerInSight)
+        {
+            playerInSight = false;
+        }
+    }
+
+    private IEnumerator FOVRoutine()
+    {
+        WaitForSeconds wait = new WaitForSeconds(0.2f);
+
+        while (true)
+        {
+            yield return wait;
+            FieldOfViewCheck();
+        }
+    }
+
+
+}
