2b361708d16cd88737e2dfdc356bace836d5c6ac
Maichelanger <miguelangelcarrascobobo@hotmail.com>
2024-03-12 10:16:36 +0100

    Added new sounds

--- Assets/Scripts/Objects/HealthPackController.cs
+++ Assets/Scripts/Objects/HealthPackController.cs
@@ -1,15 +1,33 @@
+using System.Collections;
 using UnityEngine;
 
 public class HealthPackController : MonoBehaviour
 {
     [SerializeField] private float healthAmount = 25;
+    [SerializeField] AudioClip healthPackSound;
+
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
+            audioSource.PlayOneShot(healthPackSound);
             other.GetComponent<PlayerHealthController>().Heal(healthAmount);
-            Destroy(gameObject);
+            GetComponent<MeshRenderer>().enabled = false;
+            GetComponent<Collider>().enabled = false;
+            StartCoroutine(DestroyHealthPack());
         }
+    }
+
+    IEnumerator DestroyHealthPack()
+    {
+        yield return new WaitForSeconds(1);
+        Destroy(gameObject);
     }
 }
