c816b847fbe399a5a74860958240489140d21292
Maichelanger <miguelangelcarrascobobo@hotmail.com>
2024-03-19 10:55:28 +0100

    Added WebGL support and fixed some errors

--- Assets/Scripts/UI/MainMenuController.cs
+++ Assets/Scripts/UI/MainMenuController.cs
@@ -3,11 +3,32 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour
 {
+    [SerializeField] private GameObject exitButton;
+
+#if UNITY_WEBGL
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        exitButton.SetActive(false);
+    }
+#else
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        exitButton.SetActive(true);
+    }
+#endif
+
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
             SceneManager.LoadScene(1);
         }
+    }
+
+    public void CloseGame()
+    {
+        Application.Quit();
     }
 }
